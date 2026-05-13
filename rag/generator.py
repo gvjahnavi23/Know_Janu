41e3df045c8a41b0a6b35813fe8aad05317f53a3
Jahnavi <venkatajahnavigunnam@gmail.com>
2026-05-13 21:14:11 +0530

    Updated model to phi3:mini

--- rag/generator.py
+++ rag/generator.py
@@ -6,7 +6,7 @@ from rag.prompt_builder import prompt_builder
 def generator(query):
     prompt = prompt_builder(query)
     response = ollama.generate(
-        model="tinyllama",
+        model="phi3:mini",
         prompt=prompt
     )
 
